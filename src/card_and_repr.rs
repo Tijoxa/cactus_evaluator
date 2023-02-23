461633713cf626a8f826d4355ebc159651c5695e
Tijoxa <92037181+Tijoxa@users.noreply.github.com>
2023-02-23 21:43:07 +0100

    split into different modules

---
+++ src/card_and_repr.rs
@@ -0,0 +1,125 @@
+use crate::arrays;
+use std::str::Chars;
+
+pub fn card_to_repr(card: &str) -> u32 {
+    let mut iterator: Chars = card.chars();
+    let symbol: Option<char> = iterator.next();
+    let color: Option<char> = iterator.next();
+
+    let cdhs: u32;
+    let rrrr: u32;
+    let pppppp: u32;
+    let bbbbbbbbbbbbb: u32;
+
+    match symbol {
+        Some('2') => {
+            rrrr = 0x0 << 8;
+            pppppp = arrays::PRIMES[0];
+            bbbbbbbbbbbbb = 0x1 << 16
+        }
+        Some('3') => {
+            rrrr = 0x1 << 8;
+            pppppp = arrays::PRIMES[1];
+            bbbbbbbbbbbbb = 0x1 << 17
+        }
+        Some('4') => {
+            rrrr = 0x2 << 8;
+            pppppp = arrays::PRIMES[2];
+            bbbbbbbbbbbbb = 0x1 << 18
+        }
+        Some('5') => {
+            rrrr = 0x3 << 8;
+            pppppp = arrays::PRIMES[3];
+            bbbbbbbbbbbbb = 0x1 << 19
+        }
+        Some('6') => {
+            rrrr = 0x4 << 8;
+            pppppp = arrays::PRIMES[4];
+            bbbbbbbbbbbbb = 0x1 << 20
+        }
+        Some('7') => {
+            rrrr = 0x5 << 8;
+            pppppp = arrays::PRIMES[5];
+            bbbbbbbbbbbbb = 0x1 << 21
+        }
+        Some('8') => {
+            rrrr = 0x6 << 8;
+            pppppp = arrays::PRIMES[6];
+            bbbbbbbbbbbbb = 0x1 << 22
+        }
+        Some('9') => {
+            rrrr = 0x7 << 8;
+            pppppp = arrays::PRIMES[7];
+            bbbbbbbbbbbbb = 0x1 << 23
+        }
+        Some('T') => {
+            rrrr = 0x8 << 8;
+            pppppp = arrays::PRIMES[8];
+            bbbbbbbbbbbbb = 0x1 << 24
+        }
+        Some('J') => {
+            rrrr = 0x9 << 8;
+            pppppp = arrays::PRIMES[9];
+            bbbbbbbbbbbbb = 0x1 << 25
+        }
+        Some('Q') => {
+            rrrr = 0xa << 8;
+            pppppp = arrays::PRIMES[10];
+            bbbbbbbbbbbbb = 0x1 << 26
+        }
+        Some('K') => {
+            rrrr = 0xb << 8;
+            pppppp = arrays::PRIMES[11];
+            bbbbbbbbbbbbb = 0x1 << 27
+        }
+        Some('A') => {
+            rrrr = 0xc << 8;
+            pppppp = arrays::PRIMES[12];
+            bbbbbbbbbbbbb = 0x1 << 28
+        }
+        _ => panic!("Symbol unfound"),
+    };
+
+    match color {
+        Some('C') => cdhs = 0x8 << 12,
+        Some('D') => cdhs = 0x4 << 12,
+        Some('H') => cdhs = 0x2 << 12,
+        Some('S') => cdhs = 0x1 << 12,
+        _ => panic!("Color unfound"),
+    };
+
+    cdhs + rrrr + pppppp + bbbbbbbbbbbbb
+}
+
+// pub fn repr_to_card(card_repr: u32) -> String {
+//     let color: char;
+//     let symbol: char;
+
+//     match (card_repr & 0xf000) >> 12 {
+//         0x8 => color = 'C',
+//         0x4 => color = 'D',
+//         0x2 => color = 'H',
+//         0x1 => color = 'S',
+//         _ => panic!("Invalid color in card_repr"),
+//     };
+
+//     match (card_repr & 0xf00) >> 8 {
+//         0x0 => symbol = '2',
+//         0x1 => symbol = '3',
+//         0x2 => symbol = '4',
+//         0x3 => symbol = '5',
+//         0x4 => symbol = '6',
+//         0x5 => symbol = '7',
+//         0x6 => symbol = '8',
+//         0x7 => symbol = '9',
+//         0x8 => symbol = 'T',
+//         0x9 => symbol = 'J',
+//         0xa => symbol = 'Q',
+//         0xb => symbol = 'K',
+//         0xc => symbol = 'A',
+//         _ => panic!("Invalid symbol in card_repr"),
+//     };
+
+//     let card = format!("{}{}", symbol, color);
+//     card
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,17 @@
+// Ce script est une adaptation en package Rust de l'algorithme de Cactus Kev Perfect Hash
+// qui permet d'évaluer une main de poker en lui donnant un score de 1 (meilleur) à 7462 (pire)
+// https://suffe.cool/poker/evaluator.html
+
+// +--------+--------+--------+--------+
+// |xxxbbbbb|bbbbbbbb|cdhsrrrr|xxpppppp|
+// +--------+--------+--------+--------+
+
 use pyo3::prelude::*;
 use std::cmp;
-use std::str::Chars;
-pub mod arrays;
-
-fn card_to_repr(card: &str) -> u32 {
-    let mut iterator: Chars = card.chars();
-    let symbol: Option<char> = iterator.next();
-    let color: Option<char> = iterator.next();
-
-    let cdhs: u32;
-    let rrrr: u32;
-    let pppppp: u32;
-    let bbbbbbbbbbbbb: u32;
-
-    match symbol {
-        Some('2') => {
-            rrrr = 0x0 << 8;
-            pppppp = arrays::PRIMES[0];
-            bbbbbbbbbbbbb = 0x1 << 16
-        }
-        Some('3') => {
-            rrrr = 0x1 << 8;
-            pppppp = arrays::PRIMES[1];
-            bbbbbbbbbbbbb = 0x1 << 17
-        }
-        Some('4') => {
-            rrrr = 0x2 << 8;
-            pppppp = arrays::PRIMES[2];
-            bbbbbbbbbbbbb = 0x1 << 18
-        }
-        Some('5') => {
-            rrrr = 0x3 << 8;
-            pppppp = arrays::PRIMES[3];
-            bbbbbbbbbbbbb = 0x1 << 19
-        }
-        Some('6') => {
-            rrrr = 0x4 << 8;
-            pppppp = arrays::PRIMES[4];
-            bbbbbbbbbbbbb = 0x1 << 20
-        }
-        Some('7') => {
-            rrrr = 0x5 << 8;
-            pppppp = arrays::PRIMES[5];
-            bbbbbbbbbbbbb = 0x1 << 21
-        }
-        Some('8') => {
-            rrrr = 0x6 << 8;
-            pppppp = arrays::PRIMES[6];
-            bbbbbbbbbbbbb = 0x1 << 22
-        }
-        Some('9') => {
-            rrrr = 0x7 << 8;
-            pppppp = arrays::PRIMES[7];
-            bbbbbbbbbbbbb = 0x1 << 23
-        }
-        Some('T') => {
-            rrrr = 0x8 << 8;
-            pppppp = arrays::PRIMES[8];
-            bbbbbbbbbbbbb = 0x1 << 24
-        }
-        Some('J') => {
-            rrrr = 0x9 << 8;
-            pppppp = arrays::PRIMES[9];
-            bbbbbbbbbbbbb = 0x1 << 25
-        }
-        Some('Q') => {
-            rrrr = 0xa << 8;
-            pppppp = arrays::PRIMES[10];
-            bbbbbbbbbbbbb = 0x1 << 26
-        }
-        Some('K') => {
-            rrrr = 0xb << 8;
-            pppppp = arrays::PRIMES[11];
-            bbbbbbbbbbbbb = 0x1 << 27
-        }
-        Some('A') => {
-            rrrr = 0xc << 8;
-            pppppp = arrays::PRIMES[12];
-            bbbbbbbbbbbbb = 0x1 << 28
-        }
-        _ => panic!("Symbol unfound"),
-    };
-
-    match color {
-        Some('C') => cdhs = 0x8 << 12,
-        Some('D') => cdhs = 0x4 << 12,
-        Some('H') => cdhs = 0x2 << 12,
-        Some('S') => cdhs = 0x1 << 12,
-        _ => panic!("Color unfound"),
-    };
-
-    cdhs + rrrr + pppppp + bbbbbbbbbbbbb
-}
-
-fn find_fast(mut u: u32) -> u32 {
-    u += 0xe91aaa35;
-    u ^= u >> 16;
-    u += u << 8;
-    u ^= u >> 4;
-    let b: u32 = (u >> 8) & 0x1ff;
-    let a: u32 = (u + (u << 2)) >> 19;
-    let hash: u32 = arrays::HASH_ADJUST[b as usize];
-    let c: u32 = a ^ hash;
-
-    c
-}
+mod arrays;
+mod card_and_repr;
+mod tools;
+use crate::card_and_repr::*;
 
 fn eval_5cards(c1: u32, c2: u32, c3: u32, c4: u32, c5: u32) -> u32 {
     let q: u32 = (c1 | c2 | c3 | c4 | c5) >> 16;
@@ -122,7 +29,7 @@ fn eval_5cards(c1: u32, c2: u32, c3: u32, c4: u32, c5: u32) -> u32 {
 
     // This performs a perfect-hash lookup for remaining hands
     let q2: u32 = (c1 & 0xff) * (c2 & 0xff) * (c3 & 0xff) * (c4 & 0xff) * (c5 & 0xff);
-    let result: u32 = arrays::HASH_VALUES[find_fast(q2) as usize];
+    let result: u32 = arrays::HASH_VALUES[tools::find_fast(q2) as usize];
 
     result
 }
